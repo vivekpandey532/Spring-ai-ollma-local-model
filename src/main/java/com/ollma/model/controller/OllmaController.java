a9e7a24b49517ebd23b4d6b5df61db3a3061f9e7
vivek.pandey <vivek.pandey@tavant.com>
2026-06-05 14:47:53 +0530

    add prompt template to give model system prompt to perform better

--- src/main/java/com/ollma/model/controller/OllmaController.java
+++ src/main/java/com/ollma/model/controller/OllmaController.java
@@ -1,7 +1,9 @@
 package com.ollma.model.controller;
 
+import com.ollma.model.service.KnowledgeBaseService;
 import com.ollma.model.service.OllmaService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class OllmaController {
 
     private final OllmaService ollmaService;
+    private final KnowledgeBaseService knowledgeBaseService;
 
-    public OllmaController(OllmaService ollmaService) {
+    public OllmaController(OllmaService ollmaService, KnowledgeBaseService knowledgeBaseService) {
         this.ollmaService = ollmaService;
+        this.knowledgeBaseService = knowledgeBaseService;
     }
 
+    /***
+     * API used to ask normally intract with Model.
+     * @param question Question asked with model
+     * @return Answer to user
+     */
     @GetMapping("/ask")
     public String ask(@RequestParam String question) {
         return ollmaService.ask(question);
+    }
+
+    /***
+     * API used with prompt template to interact with model
+     * @param question Question asked with model
+     * @return Answer to the user
+     */
+    @GetMapping("/java-assistant")
+    public String askJavaAssistant(@RequestParam String question) {
+        return knowledgeBaseService.ask(question);
     }
 }
